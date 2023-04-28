5e24ed43942207363dc06a08fe4df00f1dd56358
thanhdat <kimthanhdat0510@gmail.com>
2023-04-28 15:24:04 +0700

    config assetsInclude xlsx

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,12 @@ import * as path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
   server: {
     port: 3000,
   },
   resolve: {
     alias: [{ find: "~", replacement: path.resolve(__dirname, "src") }],
   },
+  assetsInclude: ["**/*.xlsx"],
 });
