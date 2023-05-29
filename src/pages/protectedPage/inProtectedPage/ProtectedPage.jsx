c99735c203f40f8571bfba9090fd1d7db2ef76b1
thanhdat <kimthanhdat0510@gmail.com>
2023-05-29 13:27:39 +0700

    fix ProtectedPage

--- src/pages/protectedPage/inProtectedPage/ProtectedPage.jsx
+++ src/pages/protectedPage/inProtectedPage/ProtectedPage.jsx
@@ -2,34 +2,37 @@ import { useSelector } from "react-redux"
 import { Navigate } from "react-router-dom"
 import NotPermit from "~/pages/protectedPage/notPermit"
 
-const CheckAdmin = ({ children }) => {
-  const isAdminRoute = window.location.pathname.startsWith("/admin")
-  const isRole = useSelector((state) => state.users.user.role)
+// const CheckAdmin = ({ children }) => {
+//   const isAuthenticated = useSelector((state) => state.users.isAuthenticated)
+//   const isAdminRoute = window.location.pathname.startsWith("/admin")
+//   const isRole = useSelector((state) => state.users.user.role)
 
-  if (
-    (isAdminRoute && isRole === "ADMIN") || // để ai có quyên admin mới vào đc trang admin
-    (!isAdminRoute && (isRole === "ADMIN" || isRole === "USER")) // để cho ai đăng nhập mới vào đc trang giỏ hang
-  ) {
-    return children
-  }
-
-  return <NotPermit />
-}
+//   if (
+//     (isAdminRoute && isRole === "ADMIN") || // để ai có quyên admin mới vào đc trang admin
+//     (!isAdminRoute && (isRole === "ADMIN" || isRole === "USER")) // để cho ai đăng nhập mới vào đc trang giỏ hang
+//   ) {
+//     return children
+//   } else {
+//     return <NotPermit />
+//   }
+// }
 
 const ProtectedPage = ({ children }) => {
   const isAuthenticated = useSelector((state) => state.users.isAuthenticated)
-
-  return (
-    <>
-      {isAuthenticated ? (
-        <>
-          <CheckAdmin>{children}</CheckAdmin>
-        </>
-      ) : (
-        <Navigate to="/login" />
-      )}
-    </>
-  )
+  const isAdminRoute = window.location.pathname.startsWith("/admin")
+  const isRole = useSelector((state) => state.users.user.role)
+  if (isAuthenticated) {
+    if (
+      (isAdminRoute && isRole === "ADMIN") ||
+      (!isAdminRoute && (isRole === "ADMIN" || isRole === "USER"))
+    ) {
+      return <>{children}</>
+    } else {
+      return <NotPermit />
+    }
+  } else {
+    ;<Navigate to="/login" />
+  }
 }
 
 export default ProtectedPage
